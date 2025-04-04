0c6e25a6462008c0f6c982c2f3b3c667d7bc3383
Jose Germán Martínez <jgermanmm@gmail.com>
2025-04-04 11:39:02 -0300

    Agregar archivos iniciales y dependencias para el proyecto gen19

---
+++ 00-variables.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")()
+
+// Variables
+
+// camelCase 
+let miVariable // declarar variable 
+console.log(miVariable)
+
+miVariable = "Hola Mundo" // asignar valor a un variable
+console.log(miVariable)
+
+const miConstante = 34 // en const se debe asignar un valor en el mismo instante de la declaración
+
+// miConstante = 12  los valores de las constantes no se pueden reasignar
+
+let mensaje = prompt("Ingresa tu mensaje: ")
+
+console.log(mensaje)
