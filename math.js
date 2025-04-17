207b6745bbb45918520fcd28bc2e9e6f73d87731
José G. Martínez <jgermanmm@gmail.com>
2025-04-17 11:19:54 -0400

    Agregar archivo math.js con funciones de suma, resta y constante PI; crear modulos.js como punto de entrada para la aplicación

---
+++ math.js
@@ -0,0 +1,18 @@
+
+// ECS6 import y export (2015)
+
+export function sumar(a, b) {
+  return a + b;
+}
+
+export const PI = 3.14
+
+
+export function restar(a, b) {
+    return a - b;
+}
+
+// sólo 1 por defecto
+export default function saludo(nombre) {
+  return `Hola ${nombre}`;
+}
---
+++ modulos.js
@@ -0,0 +1,22 @@
+
+// import { sumar, PI, restar } from "./math.js"
+import * as calculos from "./math.js"
+
+import prueba from "./math.js"
+
+console.log(calculos.PI)
+
+console.log("Hola mundo de el punto de entrada de la app")
+
+// console.log(sumar(2, 3))
+
+// console.log(PI)
+
+// console.log(restar(2, 3))
+
+function saludo(nombre) {
+    return `Hola ${nombre}`
+}
+
+console.log(saludo("Javier"))
+console.log(prueba("Javier"))
