f7e03a472bc2813d90d186fa1ef6cc8f9271aacc
José G. Martínez <jgermanmm@gmail.com>
2025-04-23 13:18:17 -0400

    Agregar script de JavaScript en index.html y archivo index.js; corregir color del párrafo en styles.css

---
+++ front/index.js
@@ -0,0 +1,8 @@
+
+console.log("Hola desde JS en un archivo externo")
+
+document.getElementById("parrafo").textContent = "Cambio en el párrafo en un archivo externo"
+
+document.getElementById("encabezado-secundario").style.color = "blue"
+
+console.log(document)
